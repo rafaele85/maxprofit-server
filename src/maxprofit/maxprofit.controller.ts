120e9e5e84be7d111fcc3111924e2e5bdd273ee8
Rafael E <raf65728@gmail.com>
2022-08-01 01:37:33 +0300

    fix the way error message returned

--- src/maxprofit/maxprofit.controller.ts
+++ src/maxprofit/maxprofit.controller.ts
@@ -5,7 +5,8 @@ import {Request, Response} from "express";
 import {getStorageEngine} from "../storageEngine/storageEngine.interface";
 
 export const maxprofitController = async (request: Request, response: Response ) => {
-    response.send(JSON.stringify({test: 2222}))
+    const str = JSON.stringify({test: 2222})
+    response.send(str)
     return
     const input: Partial<MaxProfitInput> = {
         start: request.query.start ? parseInt('' + request.query.start) : undefined,
