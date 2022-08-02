ca8e9df7209095bce867309a9a8a05aa6f9d6b1e
Rafael E <raf65728@gmail.com>
2022-08-02 19:34:27 +0300

    removed vercel-specific logic

--- src/maxprofit/maxprofit.controller.ts
+++ src/maxprofit/maxprofit.controller.ts
@@ -3,10 +3,8 @@ import {maxProfit} from "./maxprofit.service";
 import {logger} from "../util/logger";
 import {Request, Response} from "express";
 import {getStorageEngine} from "../storageEngine/storageEngine.interface";
-import {formatObjectForVercel} from "../util/formatObjectForVercel";
 
 export const maxprofitController = async (request: Request, response: Response ) => {
-    const isVercel = process.env.IS_VERCEL
     const input: Partial<MaxProfitInput> = {
         start: request.query.start ? parseInt('' + request.query.start) : undefined,
         end: request.query.end ? parseInt('' + request.query.end) : undefined,
@@ -16,11 +14,7 @@ export const maxprofitController = async (request: Request, response: Response )
     try {
         const storageEngine = getStorageEngine()
         const bestChoice = await maxProfit(input, storageEngine)
-        if (isVercel) {
-            response.json(bestChoice).end()
-        } else {
-            response.send(formatObjectForVercel(bestChoice))
-        }
+        response.json(bestChoice);
         return
     } catch (err) {
         error = (err as any).message || '' + (err as any) || error
