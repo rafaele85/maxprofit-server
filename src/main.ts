68319242fa6289862022677c7478202fa3cff7b7
Rafael E <raf65728@gmail.com>
2022-08-02 21:06:53 +0300

    cleanup console.log

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from 'cors'
 import {maxprofitController} from "./maxprofit/maxprofit.controller";
+import {logger} from "./util/logger";
 
 const main = () => {
 
@@ -14,7 +15,7 @@ const main = () => {
     const port = process.env.PORT || 5000
 
     app.listen(port, () => {
-        console.log('---listening to ', port)
+        logger.log('---listening to ', port)
     })
 }
 
